c6978a3409fbf0f1295a8cc483feff91be8b234a
Pakin <nametee@gmail.com>
2019-11-10 00:46:31 +0700

    commit homework 2019-10-28

---
+++ 2019-10-28/route/mainRoute.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()  
+const plusRoute = require('./plusRoute')
+const plusByJSONRoute = require('./plusByJSONRoute')
+const checkEvenNumberRoute = require('./checkEvenNumberRoute')
+const numbersRoute = require('./numbersRoute') 
+
+const static = (req,res) => res.send('Hello World') 
+const staticJSON = (req,res) => res.send(JSON.stringify({text:"Hello World"})) 
+const echo = (req,res) => res.send(req.query.text)
+
+router.get('/static',static)
+router.get('/staticJSON',staticJSON)
+router.get('/echo',echo)
+router.use('/plus',plusRoute)
+router.use('/plusByJSON',plusByJSONRoute)
+router.use('/checkEvenNumber',checkEvenNumberRoute)
+router.use('/number',numbersRoute)
+
+module.exports = router
