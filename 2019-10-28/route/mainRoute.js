7bdb04bd298c0094a52fe46273709545f7343e6e
Pakin <nametee@gmail.com>
2019-11-10 01:10:12 +0700

    commit

--- 2019-10-28/route/mainRoute.js
+++ 2019-10-28/route/mainRoute.js
@@ -4,6 +4,7 @@ const plusRoute = require('./plusRoute')
 const plusByJSONRoute = require('./plusByJSONRoute')
 const checkEvenNumberRoute = require('./checkEvenNumberRoute')
 const numbersRoute = require('./numbersRoute') 
+const countFieldsRoute = require('./countFieldsRoute') 
 
 const static = (req,res) => res.send('Hello World') 
 const staticJSON = (req,res) => res.send(JSON.stringify({text:"Hello World"})) 
@@ -16,5 +17,6 @@ router.use('/plus',plusRoute)
 router.use('/plusByJSON',plusByJSONRoute)
 router.use('/checkEvenNumber',checkEvenNumberRoute)
 router.use('/number',numbersRoute)
+router.use('/countFields',countFieldsRoute)
 
 module.exports = router
